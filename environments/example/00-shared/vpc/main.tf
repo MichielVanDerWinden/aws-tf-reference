c4feb3b489d3421bb25708c574a7526baffb4ff4
MichielVanDerWinden <michielvanderwinden@gmail.com>
2023-06-15 16:56:21 +0200

    Initial setup of shared stack and changing to state per functional set of modules (or application) instead of per specific resource type.

--- environments/example/00-shared/vpc/main.tf
+++ environments/example/00-shared/vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "../../../modules/vpc/"
+  source = "../../../../modules/vpc/"
 
   environment = "example"
 
@@ -9,4 +9,12 @@ module "vpc" {
   private_subnets  = ["10.250.16.0/24", "10.250.17.0/24", "10.250.18.0/24"]
   public_subnets   = ["10.250.19.0/24", "10.250.20.0/24", "10.250.21.0/24"]
   database_subnets = ["10.250.22.0/23", "10.250.24.0/23", "10.250.26.0/23"]
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "rds_sg_id" {
+  value = module.vpc.rds_sg_id
 }
